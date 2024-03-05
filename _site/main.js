13cfa2904627ed6e97ff9ac54fde1a5221fa7a17
chloe <chloe.laureate@gmail.com>
2024-03-05 11:44:20 +0900

    hi

--- _site/main.js
+++ _site/main.js
@@ -2,3 +2,20 @@ $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
     $(this).toggleClass('active');
 });
+
+
+document.addEventListener('scroll', function() {
+  const bannerHeight = document.querySelector('.banner').offsetHeight;
+  const textElements = document.querySelectorAll('.font-amandine'); // Targeting your text elements
+
+  // Check if the page has been scrolled past the height of the banner
+  if (window.scrollY > bannerHeight) {
+    textElements.forEach(function(el) {
+      el.classList.add('text-after-scroll');
+    });
+  } else {
+    textElements.forEach(function(el) {
+      el.classList.remove('text-after-scroll');
+    });
+  }
+});
